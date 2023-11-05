aa0eb756ef7347d775f75792dca7f4f0f5dac074
airickLeo <88010729+airickLeo@users.noreply.github.com>
2023-11-04 22:53:00 -0400

    Partial completion of unity integration

--- userInterface/src/App.jsx
+++ userInterface/src/App.jsx
@@ -2,13 +2,14 @@ import { ReactDOM } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import { Link } from "react-router-dom";
+import Kinematics from "./components/Kinematics";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} key={"home"}/>
-        <Route path="/kinematics" element={<></>} key={"kin"}/>
+        <Route path="/kinematics" element={<Kinematics />} key={"kin"}/>
         <Route path="/dynamics" element={<></>} key={"dynamics"}/>     
         <Route path="/collisions" element={<></>} key={"collisions"}/>
       </Routes>
--- userInterface/src/components/Kinematics.jsx
+++ userInterface/src/components/Kinematics.jsx
@@ -1,8 +1,19 @@
+import { Unity, useUnityContext } from "react-unity-webgl";
+import "./Introduction.css"
+
 const Kinematics = () => {
+
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "cannonBall/cannon-prototype-game.loader.js",
+        dataUrl: "cannonBall//cannon-prototype-game.data.unityweb",
+        frameworkUrl: "cannonBall/cannon-prototype-game.framework.js.unityweb",
+        codeUrl: "cannonBall//cannon-prototype-game.wasm.unityweb",
+    });
+
     return (
-        <>
-            hello
-        </>
+        <div className="kinematics-demo">
+            <Unity unityProvider={unityProvider}/>
+        </div>
     )
 }
 
